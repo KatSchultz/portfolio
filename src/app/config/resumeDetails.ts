1362e47a12589d6e416c9a4dcbbe649ac663bab2
KatSchultz <ks.katieschultz@gmail.com>
2023-07-08 23:09:37 -0400

    resume styling

---
+++ src/app/config/resumeDetails.ts
@@ -0,0 +1,22 @@
+export const workExperience = [
+  {
+    title: "Front End Developer",
+    employer: "eForms",
+    dates: "February 2023 - June 2023",
+    description: [
+      `Developed an invoicing app using JavaScript, React, and Redux, integrating Jira ticket management and following Agile development methodology`,
+      `Collaborated closely with design teams utilizing Figma and Material UI to ensure a seamless user experience`,
+      `Strong focus on quality assurance; created and maintained test cases and actively participated in code reviews for fellow developers`,
+    ],
+  },
+  {
+    title: "Office Administrator",
+    employer: "Real Estate in the Pointes",
+    dates: "February 2023 - June 2023",
+    description: [
+      `Developed an invoicing app using JavaScript, React, and Redux, integrating Jira ticket management and following Agile development methodology`,
+      `Collaborated closely with design teams utilizing Figma and Material UI to ensure a seamless user experience`,
+      `Strong focus on quality assurance; created and maintained test cases and actively participated in code reviews for fellow developers`,
+    ],
+  },
+];
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 
+function Divider() {
+  return <div className="border-solid border-my-teal border-b-2 my-2 w-full" />;
+}
+
 export default function ResumePage() {
   return (
-    <div>
-      <h2>Resume</h2>
-      <div>
-        <h3>Professional Experience</h3>
+    <div className="flex items-center flex-col w-full">
+      <h2 className="font-bold text-xl">RESUME</h2>
+      <div className="flex flex-col items-center md:max-w-3xl">
+        <h3 className="font-bold text-lg">Professional Experience</h3>
+        <Divider />
         <div>
-          <h4>Front End Developer</h4>
-          <h4>eForms</h4>
-          <h5>February 2023 - June 2023</h5>
-          <ul>
-            <li>
+          <div className="flex justify-between">
+            <div>
+              <h4 className="font-bold">Front End Developer</h4>
+              <h4 className="font-bold">eForms</h4>
+            </div>
+            <h5>February 2023 - June 2023</h5>
+          </div>
+          <ul className="list-disc list-inside">
+            <li className="mb-2">
               Developed an invoicing app using JavaScript, React, and Redux,
               integrating Jira ticket management and following Agile development
               methodology
