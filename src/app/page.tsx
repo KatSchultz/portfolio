523a30dd3f8e915ea81483e70a87673d92312ccf
KatSchultz <ks.katieschultz@gmail.com>
2023-08-06 15:51:01 -0400

    main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import NavBubbles from "./components/navigation/NavBubbles/NavBubbles";
 export default function Home() {
   return (
     <div className="flex w-full p-4 justify-center items-center">
-      <div className="flex ">
+      <div className="flex flex-col md:flex-row">
         <div>
           <Image
             src={headshot}
@@ -15,12 +15,12 @@ export default function Home() {
             className="rounded-full"
           ></Image>
         </div>
-        <div className="flex flex-col justify-center items-start p-2">
-          <h2 className="text-6xl">Hello</h2>
-          <p className="w-80">
+        <div className="flex flex-col justify-center items-start p-4">
+          <h2 className="text-6xl pb-4">Hello</h2>
+          <p className="max-w-sm pb-4">
             My background is in real estate and photography. Web development has
-            unlocked my creativity in ways I never expected. I`&apos`m always
-            looking forward to the next chalenge.
+            unlocked my creativity in ways I never expected. I'm always looking
+            forward to the next chalenge.
           </p>
           <NavBubbles />
         </div>
